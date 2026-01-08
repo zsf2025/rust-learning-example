fb2015bf3d75d5f80588178a2af72ecc303c20a8
zhangfu <18072150332@163.com>
2026-01-09 00:43:03 +0800

    refactore: 完结实战1

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use axum::{routing::{ get, post }, Router, serve, extract::{ Path, Query, Json }, http::StatusCode, response::IntoResponse };
+use tokio::net::TcpListener;
+use serde::{Deserialize, Serialize};
+
+// 返回 &'static str（静态字符串，Axum 自动转为 200 OK 响应）
+async fn hello_world() -> &'static str {
+    "Hello, Axum! 🚀"
+}
+
+async fn hello_name(Path(name): Path<String>) -> String {
+    format!("Hello, {}! 👋", name)
+}
+
+// Query是一个Map，所有在rust中我们使用HashMap来接收
+async fn greet(params: Query<std::collections::HashMap<String, String>>) -> String {
+    // 处理两个参数都有，通过元组匹配
+    if let (Some(name), Some(sex)) = (params.get("name"), params.get("sex")) {
+        format!("Greetings, {} the {}! 🌟", name, sex)
+    }
+    // 处理只有name
+    else if let Some(name) = params.get("name") {
+        format!("Greetings, {}! 🌟", name)
+    } 
+    // 处理只有sex
+    else if let Some(sex) = params.get("sex") {
+        format!("Greetings, {}! 🌟", sex)
+    }
+    // 处理没有参数
+    else {
+        "Greetings, stranger! 🌟".to_string()
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Payload {
+    msg: String,
+}
+
+async fn echo_json(Json(payload): Json<Payload>) -> impl IntoResponse {
+    (StatusCode::OK, Json(Payload {
+        msg: format!("Echo: {}", payload.msg),
+    }))
+}
+
+#[tokio::main]
+    // 给 main 加返回值：Result<(), 错误类型>
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 创建路由器（Router）：管理路由规则
+    let app = Router::new()
+    .route("/echo", post(echo_json))
+    .route("/greet", get(greet))
+    .route("/hello/{name}", get(hello_name))
+    // 绑定路由：GET 方法 + 路径 "/" + 处理函数 hello_world
+    .route("/", get(hello_world));
+    // 绑定端口（返回 Result，需用 ? 处理错误）
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    println!("服务器启动成功！访问：http://127.0.0.1:3000");
+    // 启动服务器
+    serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
